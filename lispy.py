1e221bd335ed1d58e933509005822291671e55a4
R3DDY97 <venkyreddy097@gmail.com>
2018-03-06 23:11:01 +0530

    added symbol n number subparsers

--- lispy.py
+++ lispy.py
@@ -2,7 +2,7 @@
 '''Lisp interpreter using python3'''
 
 import os
-# import re
+import re
 # from functools import reduce
 # from collections import deque
 from lisp_repl import repl
@@ -10,6 +10,7 @@ from lisp_globals import lisp_env
 
 ENV = lisp_env()
 LOCAL_ENV = {}
+KEY_WORDS = ['define', 'if', 'quote', 'set!', 'lambda']
 
 
 def bool_parser(lisp_str):
@@ -19,13 +20,53 @@ def bool_parser(lisp_str):
     return None
 
 def number_parser(lisp_str):
-    pass
+    lisp_str = lisp_str.strip()
+    re_number = re.match(r'^[+-]?\d+\.?\d* ', lisp_str)
+    if re_number:
+        number, lisp_str = re_number.group(), lisp_str[re_number.end():]
+    else:
+        return None
+    try:
+        if isinstance(int(number), int):
+            number = int(lisp_str)
+    except ValueError:
+        number = int(lisp_str)
+    return number, lisp_str
 
-def string_parser(lisp_str):
-    pass
+def symbol_parser(lisp_str):
+    lisp_str = lisp_str.strip()
+    re_symbol = re.match(r'^(\w+|[+-/*%]?)', lisp_str)
+    if re_symbol:
+        symbol, lisp_str = re_symbol.group(), lisp_str[re_symbol.end():]
+    else:
+        return None
+    if symbol in ENV:
+        return ENV[symbol], lisp_str
+    # elif symbol in KEY_WORDS:
+    return None
+
+def expression_parser(parsed_list, lisp_str):
+    lisp_str = lisp_str.strip()
+
 
 def input_parser(lisp_str):
-    pass
+    lisp_str = lisp_str.strip()
+    if lisp_str[0] == "(":
+        parsed_list = []
+    else:
+        print("\nsyntax error\n")
+        os.sys.exit()
+    return expression_parser(parsed_list, lisp_str)
+
+    # sub_parsers = [expression_parser, bool_parser, number_parser, symbol_parser, ]
+
+    # while lisp_str:
+    #     for sub_parser in sub_parsers:
+    #         sub_parsed = sub_parser(lisp_str)
+    #         if sub_parsed:
+    #             parsed, lisp_str = sub_parsed
+    #             parsed_list.append(parsed)
+
 
 
 
